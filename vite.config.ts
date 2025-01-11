8fd0f35195bd281cdd9924001c0321df28b17494
kanakanho <kahoshibakichi@gmail.com>
2025-01-12 00:13:19 +0900

    fix: github pages で公開するための前準備

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // eslint-disable-next-line node/prefer-global/process
+  base: process.env.GITHUB_PAGES ? 'deckgl-react-demo' : './',
 })
